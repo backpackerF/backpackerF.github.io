628d2988709101fd79ce59cf20539ae243de9eaa
backpackerF <54212481+backpackerF@users.noreply.github.com>
2019-08-19 14:55:27 +0400

    backpackerF

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ var log = document.getElementById("email").value,
     el = document.getElementById("info");
     el1 = document.getElementById("email");
     el2 = document.getElementById("password");
-    if ((log == "user@example.com") && (pas == "mercdev")) {window.location.href = "test2.html"}
+    if ((log == "user@example.com" || "User@example.com") && (pas == "mercdev")) {window.location.href = "test2.html"}
 	else  if (log == "user@example.com") {document.getElementById("info").innerHTML = "E-Mail or password is incorrect", el.classList.add("email"), el2.style.border = "1px solid red", el1.style.border = "0px solid red", el1.style.color = "black"}
 			else {document.getElementById("info").innerHTML = "E-Mail or password is incorrect", el.classList.add("email"), el1.style.color = "red", el1.style.border = "1px solid red", el2.style.border = "0px solid red"};
 }
