4d5a81fed4d48462fcb1c55b4a77731e8fbb5a80
backpackerF <54212481+backpackerF@users.noreply.github.com>
2019-10-30 23:37:52 +0400

    Update path.php

--- path.php
+++ path.php
@@ -1,5 +1,5 @@
 <?php
-echo 'Document root: '.$_SERVER['DOCUMENT_ROOT'].'<br>';
-echo 'Полный путь к скрипту и его имя: '.$_SERVER['SCRIPT_FILENAME'].'<br>';
-echo 'Имя скрипта: '.$_SERVER['SCRIPT_NAME'];
+echo 'Полный путь к каталогу: ';
+echo $_SERVER['DOCUMENT_ROOT'];
+echo '/';
 ?>
